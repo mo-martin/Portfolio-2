2e65a707124b931335e5bfef2ce6ecaf33d71b6f
mo-martin <momartin1@hotmail.co.uk>
2015-10-29 19:07:22 +0000

    Finished nav + content box's

--- styling/js/script.js
+++ styling/js/script.js
@@ -1,7 +1,4 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/* Smooth Scroll */
 jQuery(document).ready(function(){
    
     jQuery('a[href^="#"]').click(function(e) {
@@ -16,8 +13,27 @@ jQuery(document).ready(function(){
 
 });
 
+/* Responsive Nav - */
 $(document).ready(function(){
     $("button.navtoggle").click(function(){
         $(".mainlinks").slideToggle();
     });
 });
+(function($){
+  $(document).ready(function(){
+    var current_width = $(window).width();
+    if(current_width < 768){
+      $(".mainlinks").css("display","");
+    }
+  });
+  $(window).resize(function(){
+    var current_width = $(window).width();
+    if(current_width > 768){
+      $(".mainlinks").css("display","table");
+    }
+	else {
+		$(".mainlinks").css("display","");
+	}
+  });
+
+})(jQuery);
